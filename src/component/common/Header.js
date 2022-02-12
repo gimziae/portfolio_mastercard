7295d01d964e4e0714cd8b86b31b795b30bdac42
gimziae <gimziae@gmail.com>
2022-02-13 03:21:45 +0900

    초기설정

---
+++ src/component/common/Header.js
@@ -0,0 +1,9 @@
+export default function Header(props){
+    return(
+        <header>
+            <div className="inner">
+                <h1>로고</h1>                
+            </div>
+        </header>
+    )
+}
